0f85fece3f00deb3eec69cab86cfeb0788a1af45
Lúcio Cecílio de Souza Júnior <devluciocdesouza@gmail.com>
2022-08-13 15:19:43 -0300

    Comecei o exercício
    Comecei o exercício de JS intermediário

---
+++ Exercicio-Js-Intermediario/exercicio1.js
@@ -0,0 +1,24 @@
+// Exercício 1.1
+
+var  hojeVaiChover = true
+
+if (hojeVaiChover == true) {
+    console.log("Leve seu guarda-chuva, hoje vai chover")
+ } else {
+        console.log("Fique tranquilo, hoje será um dia de sol")
+}
+
+// Exercício 1.2
+
+var var1= 5 > 1, var2= 2000000 < 3000000000, var3= "Banana" == "Banana", var4= "50" === 50,var5= "carro" != "bicicleta", var6= 50 > 27, var7= 975 < 1200;
+
+// Exercício 1.3
+
+const meuCachorro = {
+    neme :"Jacaré",
+    patas : 4,
+    calda : 1,
+    principalAmigo : "Sou eu!" 
+    } 
+    console.log(meuCachorro);
+
--- exercicio1.js
+++
@@ -1,1 +0,0 @@
-var  hojeVaiChover = "Tempo nublado"
