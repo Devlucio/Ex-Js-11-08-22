7a0f7cff549d8044466407201c1825524244da0d
Lúcio Cecílio de Souza Júnior <devluciocdesouza@gmail.com>
2022-08-17 16:22:45 -0300

    Fazendo ex JS

---
+++ Exercicio-Js-Intermediario/exercicio10.js
@@ -0,0 +1,9 @@
+const valoresNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let indice = 0; indice < valoresNum.length; indice = indice+1){
+    const valoresAtual = valoresNum [indice];
+    if (valoresNum[indice] % 2 ===  0){
+        console.log(valoresAtual)
+    }
+    
+};
